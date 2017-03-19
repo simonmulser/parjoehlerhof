7dc80673a476cd6e8310632df45ffcbe0f6e493f
Simon Mulser <simonmulser@gmail.com>
2017-03-19 12:37:33 +0100

    introduce gulp to create final files

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp'),
+    fileinclude = require('gulp-file-include'),
+    rename = require('gulp-rename'),
+    path = require("path");
+
+var paths = {
+    templates: './templates'
+}
+
+gulp.task('fileinclude', function() {
+    return gulp.src(path.join(paths.templates, '*.tpl.html'))
+        .pipe(fileinclude())
+        .pipe(rename({
+            extname: ""
+        }))
+        .pipe(rename({
+            extname: ".html"
+        }))
+        .pipe(gulp.dest('./'))
+});
