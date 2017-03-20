70fa6339db694cb816407f9bf031b8a4e166cb2e
Simon Mulser <simonmulser@gmail.com>
2017-03-20 12:29:05 +0100

    use scss files

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp'),
     fileinclude = require('gulp-file-include'),
     rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
     path = require('path');
 
 var paths = {
     templates: './templates',
+    sass: './assets/sass',
     dist: './docs'
 }
 
@@ -22,4 +24,12 @@ gulp.task('fileinclude', function() {
 
 gulp.task("watch", function(){
     gulp.watch(path.join(paths.templates, '**/*.html'), gulp.series('fileinclude'));
+    gulp.watch(path.join(paths.sass, '**/*.scss'), gulp.series('sass'));
+
 })
+
+gulp.task('sass', function () {
+  return gulp.src(path.join(paths.sass, '/assets/sass/**/*.scss'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(path.join(paths.dist, '/assets/css')));
+});
